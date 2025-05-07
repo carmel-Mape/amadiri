8f5fac192156116981d28610d7b24d45a5c278a1
carmel-Mape <carmelishemezwe@gmail.com>
2025-05-07 11:11:47 +0200

    first commit

---
+++ src/main/java/com/example/amadiri/AmadiriApplication.java
@@ -0,0 +1,16 @@
+package com.amadiri.jobsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principale de l'application Spring Boot.
+ * Point d'entrée pour démarrer l'application Amadiri.
+ */
+@SpringBootApplication
+public class AmadiriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmadiriApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/amadiri/AmadiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amadiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
